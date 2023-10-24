2433b5fc3c3b1b5e1c90ef3f8a03fc6f0d4b184a
hooonzaa <145441106+hooonzaa@users.noreply.github.com>
2023-10-24 13:16:17 +0200

    Add files via upload

---
+++ scrip.js
@@ -0,0 +1,26 @@
+ function Pythagorova(){
+
+ const numberA = parseFloat(document.getElementById  ('a').value )
+const numberB = parseFloat(document.getElementById   ('b').value   )
+
+let.result = numberA 
+
+}
+if  (!isNaN) (a) &&  (!isNaN)(b)
+
+
+
+result Math.sqrt('c')
+
+
+if (true){
+    console.log('Ahoj')
+}
+
+function program(){
+if (!10) (10) 
+Let.gettresoult('F')
+
+
+
+}
